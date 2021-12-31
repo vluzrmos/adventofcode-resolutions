f0993b20972c09235ca98deff4422419023f0215
Vagner do Carmo <vluzrmos@gmail.com>
2021-12-31 20:57:53 -0300

    2021-003 - Remove split of each line as array of chars.

--- 2021/003_p1.php
+++ 2021/003_p1.php
@@ -6,15 +6,16 @@
 
 $input = file_get_contents(__DIR__.'/inputs/003.txt');
 
-/**  @var string[][] $lines */
-$lines = array_map(fn ($line) => str_split($line), preg_split('/(\r\n|\n)/', $input));
+/**  @var string[] $lines */
+$lines = preg_split('/(\r\n|\n)/', $input);
 
 function mostCommonBit($lines, $position)
 {
     $values = ["0" => 0, "1" => 0];
 
     foreach ($lines as $line) {
-        $values[$line[$position]] ++;
+        $bit = $line[$position];
+        $values[$bit]++;
     }
 
     return $values["0"] > $values["1"] ? "0" : "1";
@@ -37,7 +38,7 @@ function inverseBinary(string $binaryString)
 
 $gamma = "";
 
-foreach (range(0, count($lines[0]) - 1) as $position) {
+foreach (range(0, strlen($lines[0]) - 1) as $position) {
     $gamma .= mostCommonBit($lines, $position);
 }
 
